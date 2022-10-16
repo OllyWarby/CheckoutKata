a95b7ea1d8133df6c08fc178aa091baf89e690d0
OllyWarby <oliverwarburton123@gmail.com>
2022-10-16 18:41:24 +0100

    added functionality for the special offers aswell as unit testing to check the logic is functioning correctly, this got a bit messy in parts so if I was to refactor this solution I would handle the items without a special offer differently without having a null coalesce to create a new special offer object.

--- CheckoutKata/Checkout.cs
+++ CheckoutKata/Checkout.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,29 +11,81 @@ namespace CheckoutKata
     public class Checkout : ICheckout
     { 
         private readonly List<Item> basket;
+        private readonly List<ItemSpecialOffer> specialOffers;
+        private CalculateTotalWithSpecialOffers calculateTotalWithSpecialOffers;
 
         public Checkout() 
         {
             basket = new List<Item>(); 
+            specialOffers = new List<ItemSpecialOffer>();   
+            calculateTotalWithSpecialOffers = new CalculateTotalWithSpecialOffers();
         } 
 
         public decimal CalulateTotal()
         {
-            decimal total = basket.Sum(x => x.UnitPrice);
-            return total;   
+            decimal totalCost = calculateTotalWithSpecialOffers.CalculateTotalPriceWithSpecialOffers(basket, specialOffers);
+
+            return totalCost;
         }
 
         public void ScanItem(Item item)
         {
            basket.Add(item);
         }
-        
+
+        //this doesn't necessarily need to be in, its mainly for testing purposes as the special offers will come from data access usually.
+        public void AddSpecialOffer(ItemSpecialOffer specialOffer)
+        {
+            specialOffers.Add(specialOffer);
+        }
         public int TotalItemCount()
         {
             return basket.Count;    
         }
     }
 
+    public class CalculateTotalWithSpecialOffers: ICaluculateSpecialOffers
+    {
+        
+        public decimal CalculateTotalPriceWithSpecialOffers(List<Item> basket, List<ItemSpecialOffer> itemSpecialOffers)
+        {
+
+            //The easiest way I could think to do this would be to get a total count of each SKU in the basket then apply the logic to that
+            Dictionary<string, int> groupedTotalsBySKU = new Dictionary<string, int>(basket.GroupBy(x => x.SKU).ToDictionary(y => y.Key, y => y.Count()));
+            decimal totalPriceWithSpecialOffers = 0;
+
+            foreach (var item in groupedTotalsBySKU)
+            { 
+                var unitPrice = basket.Where(x => x.SKU == item.Key).Select(x => x.UnitPrice).FirstOrDefault();
+                int quantity = groupedTotalsBySKU.Where(x => x.Key == item.Key).Select(x => x.Value).FirstOrDefault();
+                var specialOffer = (itemSpecialOffers.Where(x => x.SKU == item.Key).FirstOrDefault() ?? new ItemSpecialOffer { NewUnitPrice = 0m, Quantity = 0, SKU = item.Key });
+                totalPriceWithSpecialOffers += ProcessSpecialOffers(unitPrice, quantity, specialOffer);
+            }
+            return totalPriceWithSpecialOffers;
+        }
+
+        private decimal ProcessSpecialOffers(decimal unitPrice, int quantity, ItemSpecialOffer specialOffer)
+        {
+            decimal totalPricebySKU = 0;
+            if (quantity < specialOffer.Quantity || specialOffer.Quantity == 0)
+            {
+                totalPricebySKU += (quantity * unitPrice);
+            }
+            else if (quantity >= specialOffer.Quantity && specialOffer.Quantity != 0)
+            {
+                int nonSpecialOfferItemsCount = quantity % specialOffer.Quantity;
+                totalPricebySKU += (nonSpecialOfferItemsCount * unitPrice);
+                int specialOfferItemsCount = quantity - nonSpecialOfferItemsCount;
+                totalPricebySKU += (specialOfferItemsCount/specialOffer.Quantity * specialOffer.NewUnitPrice);
+            }
+            return totalPricebySKU;
+
+        }
+    }
+    interface ICaluculateSpecialOffers
+    {
+       decimal CalculateTotalPriceWithSpecialOffers(List<Item> basket, List<ItemSpecialOffer> specialOffers);
+    }
     interface ICheckout
     {
         void ScanItem(Item item);
@@ -42,6 +96,13 @@ namespace CheckoutKata
     {
         public string SKU { get; set; }
         public decimal UnitPrice { get; set; }
+    }
+
+    public class ItemSpecialOffer
+    {
+        public string SKU { get; set; }
+        public int Quantity { get; set; }
+        public decimal NewUnitPrice { get; set; }
     }
 
 
--- CheckoutKataTesting/CheckoutTests.cs
+++ CheckoutKataTesting/CheckoutTests.cs
@@ -13,7 +13,7 @@ namespace CheckoutKataTesting
         {
             //Arrange
             Checkout checkout = new Checkout();
-            Item apple = new Item{ SKU = "A99", UnitPrice = 0.5m};
+            Item apple = new Item { SKU = "A99", UnitPrice = 0.5m };
 
             //Act
             checkout.ScanItem(apple);
@@ -24,7 +24,7 @@ namespace CheckoutKataTesting
         }
 
         [TestMethod]
-        public void CheckTotalCalculation()
+        public void CheckTotalCalculationWithoutSpecialOffers()
         {
             //Arrange
             Checkout checkout = new Checkout();
@@ -41,6 +41,97 @@ namespace CheckoutKataTesting
             decimal expectedPrice = 1.4m;
             Assert.AreEqual(checkout.CalulateTotal(), expectedPrice);
         }
+
+        [TestMethod]
+        public void CheckTotalCalculationWithOneSpecialOfferItem()
+        {
+            //Arrange
+            Checkout checkout = new Checkout();
+            Item apple = new Item { SKU = "A99", UnitPrice = 0.5m };
+            ItemSpecialOffer appleOffer = new ItemSpecialOffer { NewUnitPrice = 1.3m, Quantity = 3, SKU = "A99" };
+
+            //Act
+            checkout.ScanItem(apple);
+            checkout.ScanItem(apple);
+            checkout.ScanItem(apple);
+            checkout.AddSpecialOffer(appleOffer);
+
+            //Assert
+            decimal expectedPrice = 1.3m;
+            Assert.AreEqual(checkout.CalulateTotal(), expectedPrice);
+
+        }
+        [TestMethod]
+        public void CheckTotalCalculationswithOneSpecialOfferItemPlusOne()
+        {
+            //Arrange
+            Checkout checkout = new Checkout();
+            Item apple = new Item { SKU = "A99", UnitPrice = 0.5m };
+            ItemSpecialOffer appleOffer = new ItemSpecialOffer { NewUnitPrice = 1.3m, Quantity = 3, SKU = "A99" };
+
+            //Act
+            checkout.ScanItem(apple);
+            checkout.ScanItem(apple);
+            checkout.ScanItem(apple);
+            checkout.ScanItem(apple);
+            checkout.AddSpecialOffer(appleOffer);
+
+            //Assert
+            decimal expectedPrice = 1.8m;
+            Assert.AreEqual(checkout.CalulateTotal(), expectedPrice);
+        }
+        [TestMethod]
+        public void CheckTotalCalculationsWithMultipleItemsAndSpecialOffers()
+        {
+            Checkout checkout = new Checkout();
+            Item apple = new Item { SKU = "A99", UnitPrice = 0.5m };
+            Item biscuits = new Item { SKU = "B15", UnitPrice = 0.3m };
+            ItemSpecialOffer appleOffer = new ItemSpecialOffer { NewUnitPrice = 1.3m, Quantity = 3, SKU = "A99" };
+            ItemSpecialOffer biscuitsOffer = new ItemSpecialOffer { NewUnitPrice = 0.45m, Quantity = 2, SKU = "B15" };
+
+            //Act
+            checkout.ScanItem(apple);
+            checkout.ScanItem(apple);
+            checkout.ScanItem(apple);
+            checkout.ScanItem(biscuits);
+            checkout.ScanItem(biscuits);
+            checkout.AddSpecialOffer(appleOffer);
+            checkout.AddSpecialOffer(biscuitsOffer);
+
+            //Assert
+            decimal expectedPrice = 1.75m;
+            Assert.AreEqual(checkout.CalulateTotal(), expectedPrice);
+
+        }
+
+        [TestMethod]
+        public void CheckCalculationsWithMultipleSpecialOffersAndItemsWithout()
+        {
+            Checkout checkout = new Checkout();
+            Item apple = new Item { SKU = "A99", UnitPrice = 0.5m };
+            Item biscuits = new Item { SKU = "B15", UnitPrice = 0.3m };
+            Item carrot = new Item { SKU = "C40", UnitPrice = 0.6m };
+            ItemSpecialOffer appleOffer = new ItemSpecialOffer { NewUnitPrice = 1.3m, Quantity = 3, SKU = "A99" };
+            ItemSpecialOffer biscuitsOffer = new ItemSpecialOffer { NewUnitPrice = 0.45m, Quantity = 2, SKU = "B15" };
+
+            //Act
+            checkout.ScanItem(apple);
+            checkout.ScanItem(apple);
+            checkout.ScanItem(apple);
+            checkout.ScanItem(biscuits);
+            checkout.ScanItem(biscuits);
+            checkout.ScanItem(biscuits);
+            checkout.ScanItem(carrot);
+            checkout.ScanItem(carrot);
+            checkout.AddSpecialOffer(appleOffer);
+            checkout.AddSpecialOffer(biscuitsOffer);
+
+            //Assert
+            decimal expectedPrice = 3.25m;
+            Assert.AreEqual(checkout.CalulateTotal(), expectedPrice);
+        }
+
+
 
 
     }
